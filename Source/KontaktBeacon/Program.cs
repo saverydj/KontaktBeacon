2ed89caf6b8d4e889788d8e918108f09cb208ff7
Savery <saverydj@gmail.com>
2018-05-16 14:34:52 -0400

    Init

---
+++ Source/KontaktBeacon/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Net.Http.Headers;
+using KontaktBeacon.StaticTools;
+using System.Security.Permissions;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace KontaktBeacon
+{
+    class Program
+    {
+
+        private static HttpClient _client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            SetWebAddress();
+            SetHeaders();
+
+            Thread handleConsoleInputThread = new Thread(_ => HandleConsoleInput());
+            handleConsoleInputThread.IsBackground = true;
+            handleConsoleInputThread.Start();
+
+            ManualResetEvent readExistingEvent = new ManualResetEvent(false);
+            Console.CancelKeyPress += (sender, eArgs) => { readExistingEvent.Set(); eArgs.Cancel = true; };
+            readExistingEvent.WaitOne();
+        }
+
+        private static void HandleConsoleInput()
+        {
+            ConsoleKeyInfo keyinfo;
+            while (true)
+            {
+                keyinfo = Console.ReadKey(true);
+                if ((keyinfo.Modifiers & ConsoleModifiers.Control) != 0)
+                {
+                    if (keyinfo.Key == ConsoleKey.Q) GetDevices();
+                }
+            }
+        }
+
+
+        private static void SetWebAddress()
+        {
+            _client.BaseAddress = new Uri(Config.WebAddr);
+            _client.DefaultRequestHeaders.Clear();
+        }
+
+        private static void SetHeaders()
+        {
+            _client.DefaultRequestHeaders.Add("Accept", Config.AcceptHeader);
+            _client.DefaultRequestHeaders.Add("Api-Key", Config.PrivateAPIKey);
+        }
+
+        private static void GetDevices()
+        {
+            HttpResponseMessage response = _client.GetAsync("/device?q=uniqueId==2TEY").GetAwaiter().GetResult();
+            string content = JObject.Parse(response.Content.ReadAsStringAsync().GetAwaiter().GetResult()).ToString();
+            Console.Write(content);
+        }
+    }
+}
---
+++ Source/KontaktBeacon/StaticTools/Config.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Security.Permissions;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace KontaktBeacon.StaticTools
+{
+    public static class Config
+    {
+        public static string WebAddr;
+        public static string AcceptHeader;
+        public static string PrivateAPIKey;
+
+        private static void UpdateFields()
+        {
+            WebAddr = FormatPath(AppConfig("WebAddr"));
+            AcceptHeader = FormatPath(AppConfig("AcceptHeader"));
+            PrivateAPIKey = FormatPath(AppConfig("PrivateAPIKey"));
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        static Config()
+        {
+            string path = typeof(Config).Assembly.Location + ".config";
+            string dir = Path.GetDirectoryName(path);
+            string file = Path.GetFileName(path);
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = dir;
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = file;
+            watcher.Changed += new FileSystemEventHandler(OnAppConfigChanged);
+            watcher.EnableRaisingEvents = true;
+
+            UpdateFields();
+        }
+
+        private static void OnAppConfigChanged(object source, FileSystemEventArgs e)
+        {
+            try
+            {
+                UpdateFields();
+            }
+            catch (Exception ex)
+            {
+                if (!ex.Message.Contains("being used by another process"))
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        private static string FormatPartialPath(string path)
+        {
+            path = FormatPath(path);
+            if (!path.EndsWith(@"\"))
+            {
+                return path + @"\";
+            }
+            return path;
+        }
+
+        private static string FormatPath(string path)
+        {
+            return path.Replace("*here", Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+        }
+
+        private static string AppConfig(string key)
+        {
+            Configuration config = null;
+            string exeConfigPath = typeof(Config).Assembly.Location;
+            config = ConfigurationManager.OpenExeConfiguration(exeConfigPath);
+            if (config == null || config.AppSettings.Settings.Count == 0)
+            {
+                throw new Exception(String.Format("Config file {0}.config is missing or could not be loaded.", exeConfigPath));
+            }
+
+            KeyValueConfigurationElement element = config.AppSettings.Settings[key];
+            if (element != null)
+            {
+                string value = element.Value;
+                if (!string.IsNullOrEmpty(value))
+                    return value;
+            }
+            return string.Empty;
+        }
+
+        private static void SetField(string key, string value)
+        {
+            Configuration config = null;
+            string exeConfigPath = typeof(Config).Assembly.Location;
+            config = ConfigurationManager.OpenExeConfiguration(exeConfigPath);
+            if (config == null || config.AppSettings.Settings.Count == 0)
+            {
+                throw new Exception(String.Format("Config file {0}.config is missing or could not be loaded.", exeConfigPath));
+            }
+
+            KeyValueConfigurationElement element = config.AppSettings.Settings[key];
+            if (element == null) return;
+            element.Value = value;
+            config.Save();
+
+            UpdateFields();
+        }
+
+    }
+}
